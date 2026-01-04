6130bb9c29a6ef9d6ff216d55f7956e5560ccf95
The-Nebertcher <10362700+The-Nebertcher@users.noreply.github.com>
2026-01-04 00:30:38 -0700

    New Rebuild pushed on 260104_0030

--- rebuild.sh
+++ rebuild.sh
@@ -1,25 +1,20 @@
 #!/run/current-system/sw/bin/zsh
 
-# Define the log file
 LOG_FILE="nixos-switch.log"
 
 # Run nixos-rebuild and handle error detection
-if sudo nixos-rebuild switch &>"$LOG_FILE" || (cat "$LOG_FILE" | grep --color error && false); then
-    echo "Rebuild successful. Cloning to local repo and pushing to Git..."
+sudo nixos-rebuild switch &>"$LOG_FILE"
+if [ $? -eq 0 ]; then
     # Put commands to run on success here
-    mkdir -p /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S)
-    cp -aRfv /etc/nixos/* /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S)/
-    cd /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S)/
-    git add *
+    mkdir -p /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S) &>/dev/null
+    cp -aRfv /etc/nixos/* /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S)/ &>/dev/null
+    cd /mnt/backup/git_repos/nixos/$(date +%y%m%d_%H%M%S)/ &>/dev/null
+    git add * &>/dev/null
     git commit -m "New Rebuild pushed on $(date +%y%m%d_%H%M%S)"
-    git push origin main
-
-    rm -f $LOG_FILE
+    git push origin main &>/dev/null
 else
-    echo "Rebuild failed. Running error handling commands..."
-    # Print the error messages from the log
-    cat "$LOG_FILE" | grep --color error
-    rm -f $LOG_FILE
+  echo "Rebuild failed..."
+  cat "$LOG_FILE" | grep --color error && false
 fi
 
 
