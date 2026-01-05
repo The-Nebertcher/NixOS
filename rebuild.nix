900890d2999e0a3e66bc907d3f034e82db5f4bcc
The-Nebertcher <10362700+The-Nebertcher@users.noreply.github.com>
2026-01-04 19:38:38 -0700

    New Rebuild pushed on 20260104

--- rebuild.nix
+++ rebuild.nix
@@ -1,6 +1,10 @@
-#!/run/current-system/sw/bin/zsh
+with import <nixpkgs> {};
 
+writeShellScriptBin "rebuild" ''
 LOG_FILE="/tmp/nixos-switch.log"
+
+#Pre-Warm 
+rm -f /tmp/nixos-switch.log
 
 # Function to display a loading bar
 show_progress() {
@@ -9,8 +13,8 @@ show_progress() {
     local filled=$(( progress * width / 100 ))
     local empty=$(( width - filled ))
     printf "\r[" >/dev/tty
-    printf "%${filled}s" "" | tr ' ' '#' >/dev/tty
-    printf "%${empty}s" "" | tr ' ' '-' >/dev/tty
+    printf "%''${filled}s" "" | tr ' ' '#' >/dev/tty
+    printf "%''${empty}s" "" | tr ' ' '-' >/dev/tty 
     printf "] %d%%" "$progress" >/dev/tty
 }
 
@@ -21,13 +25,13 @@ if [ $? -eq 0 ]; then
   rm -f $LOG_FILE
   echo "Rebuild successful. Starting backup and git push..."
   show_progress 0
-  mkdir -p /mnt/backup/git_repos/nixos/ &>/dev/null
+  mkdir -p /mnt/backup/git_repos/nixos/$(date +%Y%m%d) &>/dev/null
   show_progress 20
 
-  cp -aRfv /etc/nixos/* /mnt/backup/git_repos/nixos/ &>/dev/null
+  cp -aRfv /etc/nixos/* /mnt/backup/git_repos/nixos/$(date +%Y%m%d)/ &>/dev/null
   show_progress 40
 
-  cd /mnt/backup/git_repos/nixos/ &>/dev/null
+  cd /mnt/backup/git_repos/nixos/$(date +%Y%m%d)/ &>/dev/null
   show_progress 60
 
   git add * &>/dev/null
@@ -71,3 +75,4 @@ EOF
   echo -e "\e[0m"
   cat "$LOG_FILE" | grep --color error && false
 fi
+''
